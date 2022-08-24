5a8a58a55e6555d4671f890962a63226568ba52f
Nasser Jamal <nasserjamal30@gmail.com>
2022-08-24 18:04:50 +0300

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    numbers = range(0, 101)
+    for i in numbers:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(f"{i}", end=" ")

dcd19fb5959c280a906d625929bf87b2050bf444
Nasser Jamal <nasserjamal30@gmail.com>
2022-08-25 16:25:14 +0300

    0-add fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as addFunc
+from add_0 import add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, addFunc.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))

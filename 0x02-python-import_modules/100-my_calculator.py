98d74abf26649e494a09e7eaca85ead1f51b5976
Nasser Jamal <nasserjamal30@gmail.com>
2022-08-25 19:45:12 +0300

    100-my calculation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,13 +7,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if(argv[2] == "+"):
-        print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
+        ans = add(int(argv[1]), int(argv[3]))
+        print("{} + {} = {}".format(int(argv[1]), int(argv[3]), ans))
     elif(argv[2] == "-"):
-        print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+        ans = sub(int(argv[1]), int(argv[3]))
+        print("{} - {} = {}".format(int(argv[1]), int(argv[3]), ans))
     elif(argv[2] == "*"):
-        print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+        ans = mul(int(argv[1]), int(argv[3]))
+        print("{} * {} = {}".format(int(argv[1]), int(argv[3]), ans))
     elif(argv[2] == "/"):
-        print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+        ans = div(int(argv[1]), int(argv[3]))
+        print("{} / {} = {}".format(int(argv[1]), int(argv[3]), ans))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)

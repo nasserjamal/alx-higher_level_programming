d3f4ad47f2b6ef619f29a8007e49574937e65b6a
Nasser Jamal <nasserjamal30@gmail.com>
2022-08-28 22:08:57 +0300

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list.copy()
+    if idx >= len(my_list):
+        return my_list.copy
+    copyOfList = my_list.copy()
+    copyOfList[idx] = element
+    return copyOfList

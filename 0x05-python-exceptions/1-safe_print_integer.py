b2c02f0527e0a7b4ad9ae489dce664b6084365c9
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-12 15:00:44 +0300

    debug

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,18 +8,3 @@ def safe_print_integer(value):
         return False
     except TypeError:
         return False
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = [0, 0]
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))

3e9eb6687d6b1c3a4a2637a1944eb6340e644e90
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-14 09:56:31 +0300

    Detailed square instantiation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module description goes here"""
+
+
+class Square:
+    """Defines what a square is????!!!"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

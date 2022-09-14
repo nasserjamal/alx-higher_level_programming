e698f299fa1e1cae6fac074bfdd41b6f1d3ec50c
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-14 10:56:01 +0300

    Print square with positions

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module description goes here"""
+
+
+class Square:
+    """Defines what a square is????!!!"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) == tuple and len(value) == 2:
+            if value[0] >= 0 and value[1] >= 0:
+                self.__position = value
+            else:
+                raise TypeError("position must be a tuple of 2 positive \
+                integers")
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for posy in range(0, self.__position[1]):
+                print("")
+            for row in range(0, self.__size):
+                for posx in range(0, self.__position[0]):
+                    print(" ", end="")
+                for column in range(0, self.__size):
+                    print("#", end="")
+                print("")

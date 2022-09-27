f5e816ac12d863e7dada54ebc1e78eef7ba2d97d
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-27 11:46:22 +0300

    My Rebeliious int class

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(obj, att, value):
+    """Checks if an attribute can be added and adds it"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, att, value)

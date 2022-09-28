982aa668c784dcdef67623524ff196a1634efe37
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-28 10:26:04 +0300

    Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module containing the student class"""
+
+
+class Student:
+    """Classdefining theproperties of a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Converts class to json"""
+        return self.__dict__
+
+
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))

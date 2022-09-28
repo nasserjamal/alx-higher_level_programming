8b87b8b0e1da34b8ddd8abac7154a9f0f1abb9f8
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-28 10:42:25 +0300

    Advanced student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module containing the student class"""
+
+
+from unittest import result
+
+
+class Student:
+    """Classdefining theproperties of a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Converts class to json"""
+        if type(attrs) == list:
+            result = {}
+            for item in attrs:
+                if item in self.__dict__:
+                    result[item] = self.__dict__[item]
+            return result
+        else:
+            return self.__dict__

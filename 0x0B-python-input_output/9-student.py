f973d5dc9da5382e54b85806c55a9059b163b10c
Nasser Jamal <nasserjamal30@gmail.com>
2022-09-28 10:26:41 +0300

    Student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,14 +12,3 @@ class Student:
     def to_json(self):
         """Converts class to json"""
         return self.__dict__
-
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))

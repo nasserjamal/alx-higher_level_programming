fb2b4d80cd4ee6ae1b3587b64ed8135e99f15dca
Nasser Jamal <nasserjamal30@gmail.com>
2022-11-28 22:01:25 +0300

    Mandatory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (n === 0 || isNaN(n)) {
-      return 1;
-    }
-    n = n * factorial(n - 1);
-    return n;
+  if (n === 0 || isNaN(n)) {
+    return 1;
   }
-  
-  console.log(factorial(parseInt(process.argv[2])));
+  n = n * factorial(n - 1);
+  return n;
+}
+
+console.log(factorial(parseInt(process.argv[2])));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
 function add (a, b) {
-    // a = process.argv[2];
-    // b = process.argv[3];
-  
-    console.log(a + b);
+  console.log(a + b);
 }
-  
+
 add(parseInt(process.argv[2]), parseInt(process.argv[3]));

0b5efe39b17a41e008df861777f55802d8888176
Nasser Jamal <nasserjamal30@gmail.com>
2022-12-17 18:24:01 +0300

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     data = {'email': sys.argv[2]}
-    response = requests.get(url)
+    response = requests.post(url, data=data)
     print(response.text)

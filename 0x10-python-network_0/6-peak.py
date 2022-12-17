db645844cd6f15ed3d4a3f490dfd84c9a61909e5
Nasser Jamal <nasserjamal30@gmail.com>
2022-12-17 11:32:57 +0300

    Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """task 6 modele"""
 
+
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
-    if not list_of_integers:
+    if list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
 
-    mid = len(list_of_integers) // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    elif list_of_integers[mid - 1] > list_of_integers[mid]:
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
-

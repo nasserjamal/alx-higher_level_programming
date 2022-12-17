fea0762fbe1331baba2b44a3c9f41748bf887e3d
Nasser Jamal <nasserjamal30@gmail.com>
2022-12-17 16:54:20 +0300

    Headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    with requests.get(url) as response:
+        print(response.headers['X-Request-Id'])
